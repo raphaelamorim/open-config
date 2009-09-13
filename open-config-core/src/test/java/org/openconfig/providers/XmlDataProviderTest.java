2a091fce68ccb4193aec52f6a52fd471c837d546
mrburton <mrburton@8ebb1a7a-847b-11de-8eb6-3b748465f2b3>
2009-09-13 23:02:15 +0000

    Implemented an XML DataProvider.

--- open-config-core/src/test/java/org/openconfig/providers/XmlDataProviderTest.java
+++ open-config-core/src/test/java/org/openconfig/providers/XmlDataProviderTest.java
@@ -2,6 +2,10 @@ package org.openconfig.providers;
 
 import junit.framework.TestCase;
 import org.openconfig.core.OpenConfigContext;
+import org.openconfig.providers.ast.NodeManager;
+import org.openconfig.providers.ast.SimpleNode;
+import org.openconfig.providers.ast.AbstractNode;
+import org.openconfig.providers.ast.ComplexNode;
 
 /**
  * @author Richard L. Burton III - SmartCode LLC
@@ -21,5 +25,11 @@ public class XmlDataProviderTest extends TestCase {
         XmlDataProvider dataProvider = new XmlDataProvider();
         dataProvider.initialize(context);
         dataProvider.reload();
+
+        ComplexNode root = dataProvider.getRoot();
+        NodeManager nodeManager  = new NodeManager();
+        assertEquals("Burton", ((SimpleNode)nodeManager.find("person.name", root)).getValue());
+        assertEquals("richard", ((SimpleNode)nodeManager.find("name", root)).getValue());
+
     }
 }
