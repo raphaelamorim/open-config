6137c7599603ea372bacd939770299dbf602b815
dushyanth.inguva <dushyanth.inguva@8ebb1a7a-847b-11de-8eb6-3b748465f2b3>
2010-01-22 08:19:17 +0000

    Adding integration test capability of server model with an inmemory hsqldb

--- open-config-server-core/src/main/java/org/openconfig/server/jmx/BasicJMXAuthenticator.java
+++ open-config-server-core/src/main/java/org/openconfig/server/jmx/BasicJMXAuthenticator.java
@@ -7,7 +7,6 @@ import javax.security.auth.Subject;
  * @author Richard L. Burton III - SmartCode LLC
  */
 public class BasicJMXAuthenticator implements JMXAuthenticator {
-    @Override
     public Subject authenticate(Object o) {
         System.out.println("o = " + o);
         return null;
---
+++ open-config-server-core/src/test/java/org/openconfig/server/integration/DefaultConfigurationServiceTest.java
@@ -0,0 +1,57 @@
+package org.openconfig.server.integration;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openconfig.server.domain.Configuration;
+import org.openconfig.server.domain.ConfigurationValue;
+import static org.openconfig.server.domain.ConfigurationValue.newConfigurationValue;
+import org.openconfig.server.domain.ValueType;
+import org.openconfig.server.service.ConfigurationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Set;
+
+/**
+ * @author Dushyanth (Dee) Inguva - SmartCode LLC
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/spring-test-config.xml")
+public class DefaultConfigurationServiceTest {
+
+    @Autowired
+    private ConfigurationService configurationService;
+
+    @Test
+    public void testSaveConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.setName("MOOOO");
+        ConfigurationValue integerValue = newConfigurationValue("integerValue", 55, ValueType.INT);
+        ConfigurationValue doubleValue = newConfigurationValue("doubleValue", 33.0, ValueType.DOUBLE);
+        ConfigurationValue stringValue = newConfigurationValue("stringValue", "test", ValueType.STRING);
+        ConfigurationValue booleanValue = newConfigurationValue("booleanValue", true, ValueType.BOOLEAN);
+        ConfigurationValue floatValue = newConfigurationValue("floatValue", 22.0f, ValueType.FLOAT);
+        ConfigurationValue charValue = newConfigurationValue("charValue", 'k', ValueType.CHAR);
+        ConfigurationValue shortValue = newConfigurationValue("shortValue", (short) 8, ValueType.SHORT);
+
+
+        configuration.addConfigurationValue(integerValue);
+        configuration.addConfigurationValue(doubleValue);
+        configuration.addConfigurationValue(stringValue);
+        configuration.addConfigurationValue(booleanValue);
+        configuration.addConfigurationValue(floatValue);
+        configuration.addConfigurationValue(charValue);
+        configuration.addConfigurationValue(shortValue);
+        Set<ConfigurationValue> beforeSaving = configuration.getValues();
+        configurationService.saveConfiguration(configuration);
+
+        Configuration persistedConfiguration = configurationService.findConfiguration("MOOOO");
+        assertEquals(configuration.getName(), persistedConfiguration.getName());
+
+        Set<ConfigurationValue> afterSaving = persistedConfiguration.getValues();
+        assertEquals(7, afterSaving.size());
+        assertEquals(beforeSaving, afterSaving);
+    }
+}
