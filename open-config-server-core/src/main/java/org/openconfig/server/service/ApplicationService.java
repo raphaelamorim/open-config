cf0cf0bece56e9e85df29894cedc95d41ddedfb7
dushyanth.inguva <dushyanth.inguva@8ebb1a7a-847b-11de-8eb6-3b748465f2b3>
2010-01-23 20:06:05 +0000

    Checking in missed files.

--- open-config-server-core/src/main/java/org/openconfig/server/service/ApplicationService.java
+++ open-config-server-core/src/main/java/org/openconfig/server/service/ApplicationService.java
@@ -4,20 +4,25 @@ import org.openconfig.server.domain.Application;
 import org.openconfig.server.domain.Configuration;
 import org.openconfig.server.repository.ApplicationRepository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.Map;
 
 /**
+ * Responsible to persist, and retreive the application.
+ *
  * @author Dushyanth (Dee) Inguva - SmartCode LLC
  */
 public class ApplicationService {
     private ApplicationRepository applicationRepository;
 
     public Application findApplication(String name) {
+        Assert.notNull(name);
         return applicationRepository.findByName(name);
     }
 
     public Map<String, Configuration> findConfigurationsByApplication(String name) {
+        Assert.notNull(name);
         Application application = applicationRepository.findByName(name);
         return application.getConfigurations();
     }
