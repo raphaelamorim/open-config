7c5b0ea51984463ec869b22512480c712aa68f7b
dushyanth.inguva <dushyanth.inguva@8ebb1a7a-847b-11de-8eb6-3b748465f2b3>
2010-01-26 05:33:30 +0000

    Enabling JMX exposing of configurations. (missed checkin)

--- open-config-server-core/src/test/java/org/openconfig/server/transformer/OpenMBeanApplicationTransformerTest.java
+++ open-config-server-core/src/test/java/org/openconfig/server/transformer/OpenMBeanApplicationTransformerTest.java
@@ -65,7 +65,7 @@ public class OpenMBeanApplicationTransformerTest {
 
     @Before
     public void setup() {
-        transformer.setTransformer(valueTransformer);
+        transformer.setValueTransformer(valueTransformer);
     }
 
 }
